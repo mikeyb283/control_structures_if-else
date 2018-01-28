427d45a817b5869c44266e9a32660832fc1ab7d9
Michael Balling <mballing@students.mchenry.edu>
2018-01-27 19:58:14 -0600

    Update ControlStructuresif_else.java

--- src/ControlStructuresif_else.java
+++ src/ControlStructuresif_else.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 public class ControlStructuresif_else {
 
   public static void main (String[] args){
+    //declaring the valid range between 92 and 104
     double highTemp = 104;
     double lowTemp = 92;
+    //variable for user input
     double userTemp;
     Scanner scanner = new Scanner(System.in);
-
+    
+    //print a message to prompt the user to enter a temperature value
     System.out.println("Please enter a temperature between " + lowTemp + " and " + highTemp);
     userTemp = scanner.nextDouble();
 
+    //testing the user input to ensure the value is within specified range
     if (userTemp > lowTemp && userTemp < highTemp){
       if(userTemp < 97.5){
         System.out.println("The temperature " + userTemp + " degrees is below normal.");
